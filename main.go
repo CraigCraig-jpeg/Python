0d8765724a2467a8849e8a1b9d08496a39c6528d
Craig <78127468+CraigCraig-jpeg@users.noreply.github.com>
2023-05-22 12:51:12 +0200

    Update main.go

--- main.go
+++ main.go
@@ -138,4 +138,36 @@
     }
   ]
 }
-
+0-----------
+    
+    "boot_command": [
+        "<esc><wait>",
+        "<esc><wait>",
+        "<enter><wait>",
+        "/install/vmlinuz<wait>",
+        " auto<wait>",
+        " console-setup/ask_detect=false<wait>",
+        " console-setup/layoutcode=us<wait>",
+        " console-setup/modelcode=pc105<wait>",
+        " debconf/frontend=noninteractive<wait>",
+        " debian-installer=en_US<wait>",
+        " fb=false<wait>",
+        " initrd=/install/initrd.gz<wait>",
+        " kbd-chooser/method=us<wait>",
+        " keyboard-configuration/layout=USA<wait>",
+        " keyboard-configuration/variant=USA<wait>",
+        " locale=en_US<wait>",
+        " netcfg/get_domain=vm<wait>",
+        " netcfg/get_hostname=ubuntu<wait>",
+        " grub-installer/bootdev=/dev/sda<wait>",
+        " noapic<wait>",
+        " preseed/file=/preseed.cfg<wait>",
+        " -- <wait>",
+        "<enter>"
+      ],
+      "floppy_files": [
+        "data/user-data.pkrtpl.hcl",
+        "data/network-config.pkrtpl.hcl"
+      ]
+    }
+  ],
