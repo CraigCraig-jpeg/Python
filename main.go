24b1c276f0c308a629037056b504a6dcbe5b4ebb
Craig <78127468+CraigCraig-jpeg@users.noreply.github.com>
2023-05-22 13:07:51 +0200

    Update main.go

--- main.go
+++ main.go
@@ -171,3 +171,93 @@
       ]
     }
   ],
+
+
+
+---------------
+
+
+{
+  "builders": [
+    {
+      "type": "vsphere-iso",
+      "vcenter_server": "vcenter.example.com",
+      "username": "username",
+      "password": "password",
+      "insecure_connection": true,
+      "vm_name": "ubuntu-server",
+      "datacenter": "datacenter-name",
+      "cluster": "cluster-name",
+      "network": "network-name",
+      "datastore": "datastore-name",
+      "convert_to_template": true,
+      "disk_size": "20000",
+      "communicator": "ssh",
+      "ssh_username": "ubuntu",
+      "ssh_password": "ubuntu",
+      "ssh_wait_timeout": "30m",
+      "ssh_port": 22,
+      "shutdown_command": "sudo systemctl poweroff",
+      "guest_os_type": "ubuntu-64",
+      "iso_paths": [
+        "[datastore-name] path/to/ubuntu-20.04.3-live-server-amd64.iso"
+      ],
+      "boot_command": [
+        "<esc><wait>",
+        "<esc><wait>",
+        "<enter><wait>",
+        "/install/vmlinuz<wait>",
+        " auto<wait>",
+        " console-setup/ask_detect=false<wait>",
+        " console-setup/layoutcode=us<wait>",
+        " console-setup/modelcode=pc105<wait>",
+        " debconf/frontend=noninteractive<wait>",
+        " debian-installer=en_US<wait>",
+        " fb=false<wait>",
+        " initrd=/install/initrd.gz<wait>",
+        " kbd-chooser/method=us<wait>",
+        " keyboard-configuration/layout=USA<wait>",
+        " keyboard-configuration/variant=USA<wait>",
+        " locale=en_US<wait>",
+        " netcfg/get_domain=vm<wait>",
+        " netcfg/get_hostname=ubuntu<wait>",
+        " grub-installer/bootdev=/dev/sda<wait>",
+        " noapic<wait>",
+        " preseed/file=/preseed.cfg<wait>",
+        " -- <wait>",
+        "<enter>"
+      ],
+      "cd_files": [
+        {
+          "content": "#cloud-config\n\nautoinstall:\n  version: 1\n  apt:\n    geoip: true\n    preserve_sources_list: false\n    primary:\n      - arches: [amd64, i386]\n        uri: http://archive.ubuntu.com/ubuntu\n      - arches: [default]\n        uri: http://ports.ubuntu.com/ubuntu-ports\n  early-commands:\n    - sudo systemctl stop ssh\n  locale: en_US\n  keyboard:\n    layout: USA\n  identity:\n    hostname: ubuntu-server\n    username: ubuntu\n    password: ubuntu\n  ssh:\n    install-server: true\n    allow-pw: true\n  packages:\n    - openssh-server\n    - open-vm-tools\n    - cloud-init\n  user-data:\n    disable_root: false\n    timezone: UTC\n  late-commands:\n    - sed -i -e 's/^#\\?PasswordAuthentication.*/PasswordAuthentication yes/g' /target/etc/ssh/sshd_config\n    - echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' > /target/etc/sudoers.d/ubuntu\n    - curtin in-target --target=/target -- chmod 440 /etc/sudoers.d/ubuntu",
+          "destination": "/root/user-data"
+        }
+      ]
+    }
+  ],
+  "provisioners": [
+    {
+      "type": "shell",
+      "execute_command": "echo 'password' | sudo -S sh '{{ .Path }}'",
+      "scripts": [
+        "scripts/01-base.sh",
+        "scripts/02-extras.sh"
+      ]
+    }
+  ],
+  "post-processors": [
+    {
+      "type": "vsphere",
+      "vcenter_server": "vcenter.example.com",
+      "username": "username",
+      "password": "password",
+      "insecure_connection": true,
+      "datacenter": "datacenter-name",
+      "cluster": "cluster-name",
+      "template_name": "ubuntu-server",
+      "vm_name": "ubuntu-server",
+      "convert_to_template": false
+    }
+  ]
+}
+
